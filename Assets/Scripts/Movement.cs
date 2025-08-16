7d2f8f655f509d4677e14d5172e69e74e0e0cee6
An <162073486+AnDayno@users.noreply.github.com>
2025-08-16 23:24:25 +0800

    Added new scene

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void OnEnable()
     {
         thrust.Enable();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         if(thrust.IsPressed())
         {
