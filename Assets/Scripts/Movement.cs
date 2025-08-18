7ee3b06ed494d533cb3149d70e023e31fa3b86f5
An <162073486+AnDayno@users.noreply.github.com>
2025-08-18 21:24:03 +0800

    Added movement rotation listener

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,9 +1,11 @@
 using UnityEngine.InputSystem;
 using UnityEngine;
+using System;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
     [SerializeField] float thrustForce = 10f;
     Rigidbody rb;
 
@@ -15,13 +17,26 @@ public class Movement : MonoBehaviour
     private void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
     }
 
     private void FixedUpdate()
     {
-        if(thrust.IsPressed())
+        ApplyThrust();
+        ApplyRotation();
+    }
+
+    private void ApplyThrust()
+    {
+        if (thrust.IsPressed())
         {
             rb.AddRelativeForce(Vector3.up * thrustForce * Time.fixedDeltaTime);
         }
     }
+    private void ApplyRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        Debug.Log($"Rotation Input: {rotationInput}");
+    }
+
 }
